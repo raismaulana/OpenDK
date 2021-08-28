695f8d6169992ac583e7a9fbd92d1e52b93201a8
afa28 <andifahruddinakas@gmail.com>
2021-08-28 17:04:35 +0800

    Laporan Penduduk

--- app/Rules/CekDesa.php
+++ app/Rules/CekDesa.php
@@ -22,7 +22,7 @@ class CekDesa implements Rule
      */
     public function __construct()
     {
-        //
+        $this->nama_kec = Profil::where('kecamatan_id', config('app.default_profile'))->first()->kecamatan->nama;
     }
 
     /**
@@ -34,7 +34,6 @@ class CekDesa implements Rule
      */
     public function passes($attribute, $value)
     {
-        $this->nama_kec = Profil::where('kecamatan_id', config('app.default_profile'))->first()->kecamatan->nama;
         $this->value = $value;
 
         return DataDesa::where($attribute, $value)->first();
